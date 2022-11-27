235b79a1d2d8deae29258f13b2013bf0b34a6a4f
Peter Petruš <petrus.peterhe@gmail.com>
2022-11-27 11:23:02 +0100

    305 - expiration token for login autologout ...

--- angular-the-complete-guide-2023-edition/recipe-book/src/app/auth/auth.service.ts
+++ angular-the-complete-guide-2023-edition/recipe-book/src/app/auth/auth.service.ts
@@ -20,6 +20,8 @@ export class AuthService {
   //user = new Subject<User>();//replace subject for behaviorsubject
   user = new BehaviorSubject<User>(null);//get previous value, for use fetch data 
 
+  private tokenExpirationTimer : any;
+
   constructor(
     private http : HttpClient,
     private router: Router) { }
@@ -76,6 +78,9 @@ export class AuthService {
     
     if(loadedUser.token){
       this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();//check rest time
+
+      this.autoLogout(expirationDuration);
     }
   
   }
@@ -83,6 +88,20 @@ export class AuthService {
   logout(){
     this.user.next(null);
     this.router.navigate(['/auth']);
+
+    localStorage.removeItem('userData');//after logout clear userData
+
+    if(this.tokenExpirationTimer){// if it's logout, check expirationtimer
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number){
+    // console.log(expirationDuration);
+   this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);//time to expire
   }
 
   private handleAuthentication(
@@ -94,7 +113,7 @@ export class AuthService {
       const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);//need to be gettime otherwise expire time is not valid
       const user = new User(email, userId, token, expirationDate);
       this.user.next(user);
-
+      this.autoLogout(expiresIn * 1000);
       localStorage.setItem('userData', JSON.stringify(user));//prevent from lose data
   }
 
