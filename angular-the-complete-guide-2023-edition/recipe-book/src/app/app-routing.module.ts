e4b24c0aa960baff5201a18456afa458729c0d54
Peter Petruš <petrus.peterhe@gmail.com>
2022-11-30 19:02:27 +0100

    324. add routing for recipes

--- angular-the-complete-guide-2023-edition/recipe-book/src/app/app-routing.module.ts
+++ angular-the-complete-guide-2023-edition/recipe-book/src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
 import { AuthComponent } from "./auth/auth.component";
-import { AuthGuard } from "./auth/auth.guard";
 import { ErrorPageComponent } from "./error-page/error-page.component";
-import { NoRecipeComponent } from "./recipes/no-recipe/no-recipe.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
-import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
@@ -16,17 +9,6 @@ const appRoutes: Routes = [
         path: "",
         redirectTo: "/recipes",
         pathMatch: 'full'
-    },
-    {
-        path: 'recipes',
-        component: RecipesComponent,
-        canActivate: [AuthGuard],
-        children: [
-            {path: '', component: NoRecipeComponent },
-            {path: 'new', component: RecipeEditComponent},
-            {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
-            {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
-        ]
     },
     {
         path: 'shopping-list',
--- angular-the-complete-guide-2023-edition/recipe-book/src/app/recipes/recipes.modute.ts
+++ angular-the-complete-guide-2023-edition/recipe-book/src/app/recipes/recipes.modute.ts
@@ -9,6 +9,7 @@ import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipesComponent } from "./recipes.component";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 
 @NgModule({
     declarations: [
@@ -22,7 +23,8 @@ import { RecipesComponent } from "./recipes.component";
     imports: [
         RouterModule,
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RecipesRoutingModule
     ],
     exports:[
         RecipeListComponent,
