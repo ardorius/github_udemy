ddcb8d8ac22872b6a80f9dd0eb80479e03a3d566
Peter Petruš <petrus.peterhe@gmail.com>
2022-12-01 19:28:50 +0100

    fix routing

--- angular-the-complete-guide-2023-edition/recipe-book/src/app/app.module.ts
+++ angular-the-complete-guide-2023-edition/recipe-book/src/app/app.module.ts
@@ -25,9 +25,9 @@ import { AuthModule } from './auth/auth.module';
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-    AppRoutingModule,
     CoreModule,
-    AuthModule
+    AuthModule,
+    AppRoutingModule,
   ],
   bootstrap: [AppComponent]
   //, entryComponents:[
