5ba265d8911efc624878273d4b9a19192b395d53
Peter Petruš <petrus.peterhe@gmail.com>
2022-12-07 21:38:33 +0100

    370. Setting Up Auth Reducer & Actions

--- angular-the-complete-guide-2023-edition/recipe-book/src/app/auth/store/auth.reducer.ts
+++ angular-the-complete-guide-2023-edition/recipe-book/src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from "../user.model";
+import * as AuthActions from "./auth.actions";
 
 export interface State{
     user: User;
@@ -10,7 +11,28 @@ const initialState = {
 
 export function authReducer(
     state = initialState, 
-    action
+    action: AuthActions.AuthActions
 ){
+    switch(action.type){
+        case AuthActions.LOGIN:
+            const user = new User(
+                action.payload.email, 
+                action.payload.userId, 
+                action.payload.token, 
+                action.payload.expirationDate);
+            return{
+                ...state,
+                // user: user
+                user // if are same, shorter written
+            };
+        case AuthActions.LOGOUT:
+            return{
+                ...state,
+                user: null
+            };
+        default:
+            return state;
+    }
+
     return state;
 }
