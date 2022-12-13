8c494fd8c187852be37ba9b7efd952ac1cdb0b77
Peter Petruš <petrus.peterhe@gmail.com>
2022-12-13 22:11:01 +0100

    386. Using the Store Devtools - new package, need to install to chrome too

--- angular-the-complete-guide-2023-edition/recipe-book/src/app/app.module.ts
+++ angular-the-complete-guide-2023-edition/recipe-book/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -17,6 +18,7 @@ import { CoreModule } from './core/core.module';
 import * as fromApp from './store/app.reducer'
 import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './auth/store/auth.effects';
+import { environment } from 'src/environments/environment';
 
 
 @NgModule({
@@ -32,7 +34,8 @@ import { AuthEffects } from './auth/store/auth.effects';
     CoreModule,
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appRedurer),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production})
   ],
   // providers: [LoggingService],
   bootstrap: [AppComponent]
