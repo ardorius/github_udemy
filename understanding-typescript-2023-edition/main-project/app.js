488efb026db32ec73b4b14862519cd4c641e3476
Peter Petruš <petrus.peterhe@gmail.com>
2023-01-06 21:04:13 +0100

    26. Function Return Types & "void"

--- understanding-typescript-2023-edition/main-project/app.js
+++ understanding-typescript-2023-edition/main-project/app.js
@@ -1,21 +1,13 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number'){
-    //     return +result;
-    // } else {
-    //     return result.toString();
-    // }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 25, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '25', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Peter', 'Mariana', 'as-text');
-console.log(combinedNames);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+function printResult2(num) {
+    console.log('Result: ' + num);
+    return;
+}
+printResult(add(5, 12));
+printResult2(add(7, 12));
+// let someValue:undefined;
--- understanding-typescript-2023-edition/main-project/app.ts
+++ understanding-typescript-2023-edition/main-project/app.ts
@@ -1,31 +1,18 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function  combine(
-    input1: Combinable, 
-    input2: Combinable, 
-    resultConversion: ConversionDescriptor){
-    let result;
-   
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }    
-    return result;
-    // if (resultConversion === 'as-number'){
-    //     return +result;
-    // } else {
-    //     return result.toString();
-    // }
-    
+function add(n1: number, n2: number) {
+    return n1 + n2;
 }
 
-const combinedAges = combine(30, 25, 'as-number');
-console.log(combinedAges);
 
-const combinedStringAges = combine('30', '25', 'as-number');
-console.log(combinedStringAges);
+function printResult(num: number): void{
+    console.log('Result: ' + num);
+}
 
-const combinedNames = combine('Peter', 'Mariana', 'as-text');
-console.log(combinedNames);
+function printResult2(num: number): undefined{
+    console.log('Result: ' + num);
+    return;
+}
+
+printResult(add(5, 12));
+printResult2(add(7, 12));
+
+// let someValue:undefined;
