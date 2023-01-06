a051116a71712822c9c6671ca2e05eb761ad89b0
Peter Petruš <petrus.peterhe@gmail.com>
2023-01-06 19:35:18 +0100

    14. Working with Numbers, Strings & Booleans

--- understanding-typescript-2023-edition/main-project/app.js
+++ understanding-typescript-2023-edition/main-project/app.js
@@ -1,1 +1,17 @@
-console.log('This is my code...');
+// console.log('This is my code...!');
+function add(n1, n2, showResult, phrase) {
+    // if(typeof n1 !== 'number' && typeof n2 !== 'number'){
+    //     throw new Error('Incorrect input!');
+    // } 
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
+// console.log(result);
