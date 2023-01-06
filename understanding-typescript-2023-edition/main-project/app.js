d5bff504f22d6de4bda5c063610026d670ba4a03
Peter Petruš <petrus.peterhe@gmail.com>
2023-01-06 20:05:40 +0100

    18. Arrays Types

--- understanding-typescript-2023-edition/main-project/app.js
+++ understanding-typescript-2023-edition/main-project/app.js
@@ -4,6 +4,13 @@
 // } = {
 var person = {
     name: 'Peter',
-    age: 34
+    age: 34,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivities; //any special type of TypeScript
+favoriteActivities = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- understanding-typescript-2023-edition/main-project/app.ts
+++ understanding-typescript-2023-edition/main-project/app.ts
@@ -5,7 +5,17 @@
 
 const person = {
     name: 'Peter',
-    age: 34
+    age: 34,
+    hobbies: ['Sports','Cooking']
 };
 
+let favoriteActivities: string[];//any special type of TypeScript
+
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map());//error
+}
