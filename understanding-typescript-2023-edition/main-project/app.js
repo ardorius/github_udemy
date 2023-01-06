67cf205f29646812d813e85095161130285feb3a
Peter Petruš <petrus.peterhe@gmail.com>
2023-01-06 20:24:44 +0100

    20. Working with Enums

--- understanding-typescript-2023-edition/main-project/app.js
+++ understanding-typescript-2023-edition/main-project/app.js
@@ -2,14 +2,36 @@
 //     name: string;
 //     age: number;
 // } = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]//tuple
+// }= {
+//     name: 'Peter',
+//     age: 34,
+//     hobbies: ['Sports','Cooking'],
+//     role: [2, 'programmer']//union type
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'Peter',
     age: 34,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'programmer'] //union type
+    role: Role.ADMIN
 };
-person.role.push('admin');
+// person.role.push('admin');//push is allow error for tuple
 // person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
 var favoriteActivities; //any special type of TypeScript
 favoriteActivities = ['Sports'];
 console.log(person.name);
@@ -17,4 +39,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     // console.log(hobby.map());//error
+}
+if (person.role === Role.ADMIN) {
+    console.log('person is admin');
 }
