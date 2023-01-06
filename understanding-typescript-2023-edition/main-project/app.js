9a8e203baf338704907356d2c1debae51a7fbdd6
Peter Petruš <petrus.peterhe@gmail.com>
2023-01-06 19:58:20 +0100

    16. Object Types

--- understanding-typescript-2023-edition/main-project/app.js
+++ understanding-typescript-2023-edition/main-project/app.js
@@ -1,20 +1,9 @@
-// console.log('This is my code...!');
-function add(n1, n2, showResult, phrase) {
-    // if(typeof n1 !== 'number' && typeof n2 !== 'number'){
-    //     throw new Error('Incorrect input!');
-    // } 
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    return n1 + n2;
-}
-// let number1: number = 5;// redundance
-var number1;
-number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-// resultPhrase = 0; //not allowed another type - checking types
-add(number1, number2, printResult, resultPhrase);
-// console.log(result);
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+var person = {
+    name: 'Peter',
+    age: 34
+};
+console.log(person.name);
