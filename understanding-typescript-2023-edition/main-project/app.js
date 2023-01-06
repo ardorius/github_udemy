be4fe7806d9f0fbcdffd35907c8bafefbb773d5d
Peter Petruš <petrus.peterhe@gmail.com>
2023-01-06 21:16:59 +0100

    28. Function Types & Callbacks

--- understanding-typescript-2023-edition/main-project/app.js
+++ understanding-typescript-2023-edition/main-project/app.js
@@ -8,12 +8,19 @@ function printResult2(num) {
     console.log('Result: ' + num);
     return;
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 12));
 printResult2(add(7, 12));
 // let combineValues: Function;
-var combineValues;
+var combineValues; //function type
 combineValues = add;
 // combineValues = printResult;
 // combineValues = 5;
 console.log(combineValues(8, 8));
 // let someValue:undefined;
+addAndHandle(10, 20, function (result) {
+    console.log(result - 5);
+});
--- understanding-typescript-2023-edition/main-project/app.ts
+++ understanding-typescript-2023-edition/main-project/app.ts
@@ -12,6 +12,11 @@ function printResult2(num: number): undefined{
     return;
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void){
+    const result = n1 + n2;
+    cb(result);
+}
+
 printResult(add(5, 12));
 printResult2(add(7, 12));
 
@@ -25,3 +30,7 @@ combineValues = add;
 console.log(combineValues(8,8));
 
 // let someValue:undefined;
+
+addAndHandle(10, 20, (result)=> {
+    console.log(result - 5);
+});
