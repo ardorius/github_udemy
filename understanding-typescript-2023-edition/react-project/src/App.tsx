ebb403d435f16b8064f6b3abfc607e92ce5c96ab
Peter Petruš <petrus.peterhe@gmail.com>
2023-02-12 15:00:13 +0100

    181. Cross-Component Communication

--- understanding-typescript-2023-edition/react-project/src/App.tsx
+++ understanding-typescript-2023-edition/react-project/src/App.tsx
@@ -1,15 +1,17 @@
 //178. How Do React + TypeScript Work Together?
-import React from 'react';
+import React, {useState} from 'react';
 
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 
 const App: React.FC = () => {
-  const todos = [{id: 't1', text: 'Finish the course'}];
+  //182. Working with State & Types
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   //181. Cross-Component Communication
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos([{id: Math.random().toString(), text: text}]);
   };
 
   return <div className='App'>
