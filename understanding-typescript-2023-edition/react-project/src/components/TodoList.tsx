cf8613ae5ab79af70829f8564977b50c50acf26e
Peter Petruš <petrus.peterhe@gmail.com>
2023-02-12 14:25:05 +0100

    179. Working with Props and Types for Props

---
+++ understanding-typescript-2023-edition/react-project/src/components/TodoList.tsx
@@ -0,0 +1,19 @@
+//179. Working with Props and Types for Props
+
+import React from "react";
+
+interface TodoListProps {
+    items: {id: string, text: string}[];
+}
+
+const TodoList: React.FC<TodoListProps> = (props) => {
+  return (
+    <ul>
+      {props.items.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
