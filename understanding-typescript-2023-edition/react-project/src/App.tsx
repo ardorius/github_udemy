ec441d81bce3369160e5951e419a30daef1f4705
Peter Petruš <petrus.peterhe@gmail.com>
2023-02-12 15:04:10 +0100

    83. Managing State Better

--- understanding-typescript-2023-edition/react-project/src/App.tsx
+++ understanding-typescript-2023-edition/react-project/src/App.tsx
@@ -1,9 +1,9 @@
 //178. How Do React + TypeScript Work Together?
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import TodoList from './components/TodoList';
-import NewTodo from './components/NewTodo';
-import { Todo } from './todo.model';
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
   //182. Working with State & Types
@@ -11,13 +11,19 @@ const App: React.FC = () => {
 
   //181. Cross-Component Communication
   const todoAddHandler = (text: string) => {
-    setTodos([{id: Math.random().toString(), text: text}]);
+    ////183. Managing State Better
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
 
-  return <div className='App'>
-    <NewTodo onAddTodo={todoAddHandler} />
-    <TodoList items={todos} />
-  </div>;
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
+    </div>
+  );
 };
 
 export default App;
