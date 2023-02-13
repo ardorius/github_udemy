88a446534710c99c129af80e04fbb8fe5e86c7e0
Peter Petruš <petrus.peterhe@gmail.com>
2023-02-13 19:28:07 +0100

    195. More CRUD Operations

--- understanding-typescript-2023-edition/nodejs-express/dist/controllers/todos.js
+++ understanding-typescript-2023-edition/nodejs-express/dist/controllers/todos.js
@@ -1,7 +1,7 @@
 "use strict";
 // 194. Working with Controllers & Parsing Request Bodies
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.deleteTodo = exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 const todo_1 = require("../models/todo");
 const TODOS = [];
 const createTodo = (req, res, next) => {
@@ -11,3 +11,30 @@ const createTodo = (req, res, next) => {
     res.status(201).json({ message: 'Created the todo.', createTodo: newTodo });
 };
 exports.createTodo = createTodo;
+const getTodos = (req, res, next) => {
+    res.json({ todos: TODOS });
+};
+exports.getTodos = getTodos;
+// 195. More CRUD Operations
+const updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updatedText = req.body.text;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("Cound not find todo!");
+    }
+    TODOS[todoIndex] = new todo_1.Todo(TODOS[todoIndex].id, updatedText);
+    res.json({ message: 'Updated!', updateTodo: TODOS[todoIndex] });
+};
+exports.updateTodo = updateTodo;
+const deleteTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updatedText = req.body.text;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("Cound not find todo!");
+    }
+    TODOS.splice(todoIndex, 1);
+    res.json({ message: 'Todo Deleted!' });
+};
+exports.deleteTodo = deleteTodo;
