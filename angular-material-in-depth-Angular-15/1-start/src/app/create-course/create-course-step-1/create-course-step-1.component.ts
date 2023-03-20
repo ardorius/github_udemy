e90b38732c2ed2ab08e050261d18427c36a652df
Peter Petruš <petrus.peterhe@gmail.com>
2023-03-20 22:15:11 +0100

    18. Angular Material Tooltip - Several Useful Options

--- angular-material-in-depth-Angular-15/1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ angular-material-in-depth-Angular-15/1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,45 +1,44 @@
-import {Component} from '@angular/core';
-import {UntypedFormBuilder, Validators} from '@angular/forms';
-import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
+import { Component } from "@angular/core";
+import { UntypedFormBuilder, Validators } from "@angular/forms";
+import { MatCalendarCellClassFunction } from "@angular/material/datepicker";
 
-const SAMPLE_TEXT =  "LOREM IPSUM .ASDASD ASDIJODSFHOADJHG JOAFHGJO HFDOJGHOJDSFHGJSFDBNGJIBFSDJIOGBJFDSPG IKJFOSDPMG KMPOCAKSOPMC  KFOPDKS";
+const SAMPLE_TEXT =
+  "LOREM IPSUM .ASDASD ASDIJODSFHOADJHG JOAFHGJO HFDOJGHOJDSFHGJSFDBNGJIBFSDJIOGBJFDSPG IKJFOSDPMG KMPOCAKSOPMC  KFOPDKS";
 
 @Component({
   selector: "create-course-step-1",
-  templateUrl:"create-course-step-1.component.html",
-  styleUrls: ["create-course-step-1.component.scss"]
+  templateUrl: "create-course-step-1.component.html",
+  styleUrls: ["create-course-step-1.component.scss"],
 })
 export class CreateCourseStep1Component {
-
   form = this.fb.group({
-    title: ['', [
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(60)
-    ]],
-    releasedAt: [new Date(1990,0,1), Validators.required],
-    category: ['BEGINNER', Validators.required],
-    courseType: ['premium', Validators.required],
+    title: [
+      "",
+      [Validators.required, Validators.minLength(5), Validators.maxLength(60)],
+    ],
+    releasedAt: [new Date(1990, 0, 1), Validators.required],
+    category: ["BEGINNER", Validators.required],
+    courseType: ["premium", Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: [SAMPLE_TEXT, [Validators.required, Validators.minLength(3), Validators.maxLength(300)]]
+    longDescription: [
+      SAMPLE_TEXT,
+      [Validators.required, Validators.minLength(3), Validators.maxLength(300)],
+    ],
   });
-//15. Angular Material Date Picker - Highlighting a Calendar Date
+  //15. Angular Material Date Picker - Highlighting a Calendar Date
   dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
     const date = cellDate.getDate();
     // console.log(cellDate, view)
-    if(view == 'month') {
+    if (view == "month") {
       // console.log('date', date)
-      return (date == 1) ? 'highlight-date' : '';
+      return date == 1 ? "highlight-date" : "";
     }
-    return '';
-  }
+    return "";
+  };
 
-  constructor(private fb: UntypedFormBuilder) {
-
-  }
+  constructor(private fb: UntypedFormBuilder) {}
 
   get courseTitle() {
-    return this.form.controls['title'];
+    return this.form.controls["title"];
   }
-
 }
