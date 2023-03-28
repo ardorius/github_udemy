a1c3f97db38b0f571defacf5438c27912752f77f
Peter Petruš <petrus.peterhe@gmail.com>
2023-03-28 18:27:53 +0200

    31. Angular Material Drag and Drop - Complete Step-by-Step Example

--- angular-material-in-depth-Angular-15/1-start/src/app/drag-drop/drag-drop.component.ts
+++ angular-material-in-depth-Angular-15/1-start/src/app/drag-drop/drag-drop.component.ts
@@ -90,4 +90,14 @@ export class DragDropComponent {
     }
   ];
 
+  drop(event: CdkDragDrop<Lesson[]>) {
+
+    console.log("previousIndex = " + event.previousIndex);
+    console.log("currentIndex = " + event.currentIndex);
+
+    moveItemInArray(this.lessons, event.previousIndex, event.currentIndex);
+
+  }
+
+
 }
