2ccec21b622f320810727acd0e019d0888a75a0c
Peter Petruš <petrus.peterhe@gmail.com>
2023-03-30 18:49:42 +0200

    35. The Angular Material Responsive Breakpoint Observer Service

--- angular-material-in-depth-Angular-15/1-start/src/app/courses-card-list/courses-card-list.component.ts
+++ angular-material-in-depth-Angular-15/1-start/src/app/courses-card-list/courses-card-list.component.ts
@@ -3,6 +3,7 @@ import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { editCourseDialog } from '../course-dialog/course-dialog.component';
 import { filter } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'courses-card-list',
@@ -14,23 +15,59 @@ export class CoursesCardListComponent implements OnInit {
   @Input()
   courses: Course[];
 
+  cols = 3;
+  rowHeight = '500px';
+
   constructor(
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private responsive: BreakpointObserver
   ) {
   }
-
+  // 35. The Angular Material Responsive Breakpoint Observer Service
   ngOnInit() {
-
+    this.responsive.observe(
+      [
+        Breakpoints.TabletPortrait,
+        Breakpoints.TabletLandscape,
+        Breakpoints.HandsetPortrait,
+        Breakpoints.HandsetLandscape
+      ]
+    )
+      .subscribe(
+        result => {
+          const breakpoints = Object.keys(result.breakpoints);
+          const activeBreakpoint = breakpoints.find(breakpoint => result.breakpoints[breakpoint]);
+          console.log("activeBreakpoint:", activeBreakpoint);
+          switch (activeBreakpoint) {
+            case Breakpoints.HandsetPortrait:
+              this.cols = 1;
+            case Breakpoints.HandsetLandscape:
+              this.cols = 1;
+              this.rowHeight = '430px';
+              break;
+              case Breakpoints.TabletPortrait:
+                this.cols = 1;
+              case Breakpoints.TabletLandscape:
+                // console.log("TabletLandscape:", this.rowHeight);
+                this.cols = 2;
+              // this.rowHeight = '600px';
+              break;
+            default:
+              this.cols = 3;
+              this.rowHeight = '500px';
+          }
+        }
+      );
   }
 
   editCourse(course: Course) {
     // 29. Angular Material Dialog - Final Implementation and Demo
     editCourseDialog(this.dialog, course)
-    .pipe(
-      filter(val => !!val)
-    )
+      .pipe(
+        filter(val => !!val)
+      )
       .subscribe(val => console.log("new course value:", val)
-    );
+      );
 
   }
 
