326a223e9a7857e8e5d74fdfe56e96e88af80951
Peter Petruš <petrus.peterhe@gmail.com>
2023-05-08 22:45:38 +0200

    9. Angular view Layer Patterns - Smart vs Presentational Components

--- reactive-angular-course/1-start/src/app/home/home.component.ts
+++ reactive-angular-course/1-start/src/app/home/home.component.ts
@@ -2,9 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Course, sortCoursesBySeqNo} from '../model/course';
 import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
 import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 import { CoursesService } from '../services/courses.services';
 
 
@@ -21,8 +18,8 @@ export class HomeComponent implements OnInit {
 
 
   constructor(
-    private coursesService: CoursesService,
-    private dialog: MatDialog) {
+    private coursesService: CoursesService
+  ) {
 
   }
 
@@ -44,19 +41,7 @@ export class HomeComponent implements OnInit {
 
   }
 
-  editCourse(course: Course) {
 
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
-  }
 
 }
 
