cf2e03fea58a21c4ce3f9ee7a1c83f1b110a434a
Peter Petruš <petrus.peterhe@gmail.com>
2023-05-16 22:09:18 +0200

    13. Loading Service Reactive API Design

--- reactive-angular-course/1-start/src/app/loading/loading.service.ts
+++ reactive-angular-course/1-start/src/app/loading/loading.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class LoadingService{
+
+  loading$: Observable<boolean>;
+  // 13. Loading Service Reactive API Design
+  showLoaderUnitCompleted<T>(obs$: Observable<T>): Observable<T> {
+    return undefined;
+  }
+
+  loadingOn(){
+
+  }
+
+  loadingOff(){
+
+  }
 
 }
