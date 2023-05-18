06b3d824654d5ee67a333463ca6ed9ad4766fa7b
Peter Petruš <petrus.peterhe@gmail.com>
2023-05-18 13:17:01 +0200

    23. Step-by-Step Implementation of an Angular Store Service

--- reactive-angular-course/1-start/src/app/services/courses.store.ts
+++ reactive-angular-course/1-start/src/app/services/courses.store.ts
@@ -2,23 +2,54 @@
 
 import { Injectable } from "@angular/core";
 import { Course, sortCoursesBySeqNo } from "../model/course";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { BehaviorSubject, Observable, throwError } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
+import { LoadingService } from "../loading/loading.service";
+import { MessagesService } from "../messages/messages.service";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root",
 })
 export class CoursesStore {
-  courses$: Observable<Course[]>;
+  // 23. Step-by-Step Implementation of an Angular Store Service
+  private subject = new BehaviorSubject<Course[]>([]);
+
+  courses$: Observable<Course[]> = this.subject.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private loading: LoadingService,
+    private messages: MessagesService
+  ) {
+    this.loadAllCourses();
+  }
+
+  private loadAllCourses(){
+
+   const loadCourses$ = this.http.get<Course[]>('/api/courses')
+    .pipe(
+      map(response => response["payload"]),
+      catchError(err => {
+        const message = "Could not load courses";
+        this.messages.showErrors(message);
+        console.log(message, err)
+        return throwError(err);
+      }),
+      tap(courses => this.subject.next(courses))
+    );
+
+    this.loading.showLoaderUnitCompleted(loadCourses$)
+      .subscribe();
+  }
 
   filterByCategory(category: string): Observable<Course[]> {
     return this.courses$.pipe(
-      map(courses =>
-          courses.filter((course) => course.category == category)
+      map((courses) =>
+        courses
+          .filter((course) => course.category == category)
           .sort(sortCoursesBySeqNo)
       )
     );
   }
-
-  constructor(parameters) {}
 }
