dabc933c95ac97bd14e21be8a7c78b96b196cb57
Peter Petruš <petrus.peterhe@gmail.com>
2023-05-19 22:15:01 +0200

    29. Authentication Store - Browser Refresh support with Local Storage

--- reactive-angular-course/1-start/src/app/services/auth.store.ts
+++ reactive-angular-course/1-start/src/app/services/auth.store.ts
@@ -1,14 +1,16 @@
 // 26. Authentication State Management - Section Introduction
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
-import { User } from '../model/user';
+import { User } from "../model/user";
 import { map, shareReplay, tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
+// 29. Authentication Store - Browser Refresh support with Local Storage
+const AUTH_DATA = "auth_data";
+
 @Injectable({
   providedIn: "root",
 })
 export class AuthStore {
-
   // 27. Authentication Store - Step-By-Step Implementation
   private subject = new BehaviorSubject<User>(null);
 
@@ -16,26 +18,30 @@ export class AuthStore {
   isLoggedIn$: Observable<boolean>;
   isLoggedOut$: Observable<boolean>;
 
-  constructor(private http:HttpClient){
-    this.isLoggedIn$ = this.user$.pipe(
-      map(user => !!user)
-    );
+  constructor(private http: HttpClient) {
+    this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
 
-    this.isLoggedOut$ = this.user$.pipe(
-      map(loggedIn => !loggedIn)
-    );
+    this.isLoggedOut$ = this.user$.pipe(map((loggedIn) => !loggedIn));
+
+    const user = localStorage.getItem(AUTH_DATA);
+
+    if(user) {
+      this.subject.next(JSON.parse(user));
+    }
   }
 
-  login(email: string, password: string): Observable<User>{
-    return this.http.post<User>('/api/login',{email, password})
-    .pipe(
-      tap(user => this.subject.next(user)),
+  login(email: string, password: string): Observable<User> {
+    return this.http.post<User>("/api/login", { email, password }).pipe(
+      tap((user) => {
+        this.subject.next(user);
+        localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+      }),
       shareReplay()
     );
   }
 
-  logout(){
+  logout() {
     this.subject.next(null);
+    localStorage.removeItem(AUTH_DATA);
   }
-
 }
