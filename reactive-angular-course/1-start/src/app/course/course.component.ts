de751b3de675f288982ba666463b218f539cfe39
Peter Petruš <petrus.peterhe@gmail.com>
2023-05-20 23:15:40 +0200

    37. Single Data Observable Pattern - Default Data Values

--- reactive-angular-course/1-start/src/app/course/course.component.ts
+++ reactive-angular-course/1-start/src/app/course/course.component.ts
@@ -1,6 +1,12 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Course } from "../model/course";
 import {
   debounceTime,
   distinctUntilChanged,
@@ -11,11 +17,20 @@ import {
   concatMap,
   switchMap,
   withLatestFrom,
-  concatAll, shareReplay, catchError
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError, combineLatest} from 'rxjs';
-import {Lesson} from '../model/lesson';
-import { CoursesService } from '../services/courses.services';
+  concatAll,
+  shareReplay,
+  catchError,
+} from "rxjs/operators";
+import {
+  merge,
+  fromEvent,
+  Observable,
+  concat,
+  throwError,
+  combineLatest,
+} from "rxjs";
+import { Lesson } from "../model/lesson";
+import { CoursesService } from "../services/courses.services";
 
 // 36. Reactive Angular - The Single Data Observable Pattern
 interface CourseData {
@@ -24,54 +39,42 @@ interface CourseData {
 }
 
 @Component({
-  selector: 'course',
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  selector: "course",
+  templateUrl: "./course.component.html",
+  styleUrls: ["./course.component.css"],
 })
 export class CourseComponent implements OnInit {
   // 34. Consolidation Exercise - Implementing the Course Screen in Reactive Style
 
   data$: Observable<CourseData>;
 
-
   constructor(
     private route: ActivatedRoute,
-    private coursesService:CoursesService) {
-
-
-  }
+    private coursesService: CoursesService
+  ) {}
 
   ngOnInit() {
-
-    const courseId = parseInt(this.route.snapshot.paramMap.get('courseId'));
+    const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
     // 36. Reactive Angular - The Single Data Observable Pattern
-    const course$ = this.coursesService.loadCourseById(courseId);
+    const course$ = this.coursesService
+      .loadCourseById(courseId)
+      // 37. Single Data Observable Pattern - Default Data Values
+      .pipe(startWith(null));
     // 35. Course Component Finished - Introduction to the Single Data Observable Pattern
-    const lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+    const lessons$ = this.coursesService
+      .loadAllCourseLessons(courseId)
+      // 37. Single Data Observable Pattern - Default Data Values
+      .pipe(startWith([]));
 
     // 36. Reactive Angular - The Single Data Observable Pattern
-    this.data$ = combineLatest([course$, lessons$])
-      .pipe(
-        map(([course, lessons]) => {
-          return {
-            course,
-            lessons
-          }
-        }),
-        tap(console.log)
-      )
+    this.data$ = combineLatest([course$, lessons$]).pipe(
+      map(([course, lessons]) => {
+        return {
+          course,
+          lessons,
+        };
+      }),
+      tap(console.log)
+    );
   }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
